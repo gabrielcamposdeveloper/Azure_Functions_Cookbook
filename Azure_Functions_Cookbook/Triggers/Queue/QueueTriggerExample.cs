0f0b0b192362ab2aedab9aabbed7be28fd5b19bf
gabrielcamposdev <gabrielcampos.dev@outlook.com>
2026-07-18 00:32:52 -0300

    ADD / Include QueueTrigger Example

---
+++ Azure_Functions_Cookbook/Triggers/Queue/QueueTriggerExample.cs
@@ -0,0 +1,13 @@
+using Microsoft.Azure.Functions.Worker;
+
+namespace AzureFunctionsCookbook;
+
+public class QueueTriggerExample
+{
+    [Function(nameof(QueueTriggerExample))]
+    public void Run(
+        [QueueTrigger("orders")] string message)
+    {
+        Console.WriteLine($"Message received: {message}");
+    }
+}
